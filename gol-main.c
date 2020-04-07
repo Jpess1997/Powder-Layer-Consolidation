a6bc2e52c30efd2d24d4981415de1de2649f82f4
Jacob Pessin <Jacob@Jacobs-MacBook-Pro-2.local>
2020-04-07 18:45:40 -0400

    Added global variables to main

--- gol-main.c
+++ gol-main.c
@@ -1,1 +1,8 @@
 //Main function code to run on the CPU
+//Jacob Pessin & Alex Vest, April 2020 Parallel Programming final project
+
+unsigned char coordinates, elements, nel, nnodes, basename, np;
+
+unsigned char psi, LM, irow, icol, nzmax, phi;
+
+extern unsigned char 
